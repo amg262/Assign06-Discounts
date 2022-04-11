13a2d13a70d6338219bbd6d9dad62b25be015be2
Andrew <andrewmgunn26@gmail.com>
2022-04-10 20:15:42 -0500

    init

---
+++ Models/NorthwindContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace Northwind.Models
+{
+    public class NorthwindContext : DbContext
+    {
+        public NorthwindContext(DbContextOptions<NorthwindContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
+    }
+}
